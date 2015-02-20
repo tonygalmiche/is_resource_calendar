c6726a66820fc703253e4c9c794be2a9f99a28e4
Asma <asma@saharaips.com>
2015-02-20 13:17:00 +0000

    CDC Gestion des commandes client

--- is_api.py
+++ is_api.py
@@ -15,6 +15,8 @@ class is_api(osv.osv):
     _name = 'is.api'
     
     def num_closing_days(self, cr, uid, partner_id, context=None):
+        """ Retourner les jours de fermetures de l'usine 
+        """
         jours_fermes = []
         if partner_id.close_monday:
             jours_fermes.append(1)
@@ -34,6 +36,8 @@ class is_api(osv.osv):
     
 
     def get_leave_dates(self, cr, uid, partner_id, context=None):
+        """ Retourner les jours de congé de l'usine 
+        """
         leave_dates = []
         if partner_id.calendar_line:
             for line in partner_id.calendar_line:                                                                                                                                                            
@@ -43,8 +47,21 @@ class is_api(osv.osv):
                     leave_dates.append(date.strftime('%Y-%m-%d'))
         return leave_dates
     
+    def get_day_except_weekend(self, cr, uid, date, num_day, context=None):
+        """ Calculer la date d'expédition en exceptant les weekends
+        """
+        if int(num_day) not in [0, 6]:
+            return date
+        else:
+            date = datetime.datetime.strptime(date, '%Y-%m-%d') - datetime.timedelta(days=1)
+            date = date.strftime('%Y-%m-%d')
+            num_day = time.strftime('%w', time.strptime(date, '%Y-%m-%d'))
+            return self.get_day_except_weekend(cr, uid, date, num_day, context=context)
 
+        
     def get_working_day(self, cr, uid, date, num_day, jours_fermes, leave_dates, context=None):
+        """ Déterminer la date d'expédition en fonction des jours de fermeture de l'usine ou des jours de congés de l'usine 
+        """
         if int(num_day) not in jours_fermes and date not in leave_dates:
             return date
         else:
